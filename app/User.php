b0967ffc5f45f1eba26743d1462a5d0b9722c799
RenatoConfuorto <renato.confuorto@outlook.it>
2022-08-01 17:10:55 +0200

    tabella users, seeder per generare users per debug

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Faker\Generator as Faker;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,17 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function generateUserCode(Faker $faker){
+        $regex = '[A-Za-z0-9]{10}';
+        $code = $faker->regexify($regex);
+        $relative_user = User::where('code', '=', $code)->first();
+
+        while ($relative_user) {
+            $code = $faker->regexify($regex);
+            $relative_user = User::where('code', '=', $code)->first();
+        }
+
+        return $code;
+    }
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        //svuotare la tabella
+        DB::table('users')->truncate();
+
+        //password uguale per tutti gli utenti per il debug
+        $password = 'password';
+
+        //creare 50 utenti + admin
+
+        //ADMIN
+        $admin = new User();
+        $admin->name = 'Admin';
+        $admin->code = $admin->generateUserCode($faker);
+        $admin->email = 'admin@gmail.com';
+        $admin->password = Hash::make($password);
+        $admin->is_admin = 1; //impostato manualmente perchè il valore di default è 0;
+
+        $admin->save();
+
+        //utenti
+        for ($i=1; $i < 51; $i++) { 
+            $user = new User();
+            $user->name = 'User-' . $i;
+            $user->code = $user->generateUserCode($faker);
+            $user->email = 'user-' . $i . '@gmail.com';
+            $user->password = Hash::make($password);
+
+            $user->save();
+        }
+    }
+}
+
