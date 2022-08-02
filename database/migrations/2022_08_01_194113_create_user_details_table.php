7b8cc270327a962af9925a77f0fa029fd934db7a
RenatoConfuorto <renato.confuorto@outlook.it>
2022-08-02 06:44:57 +0200

    aggiunta tabella user_details

---
+++ database/migrations/2022_08_01_194113_create_user_details_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->string('name', 50);
+            $table->string('last_name', 50)->nullable();
+            $table->string('username', 50)->nullable();
+            $table->string('country', 100)->nullable();
+            $table->string('city', 100)->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+}
---
+++ database/seeds/UserDetailsTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\User;
+use App\UserDetail;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\DB;
+
+class UserDetailsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        DB::table('user_details')->truncate();
+        //prelevare gli utenti
+        $users = User::all();
+
+        foreach ($users as $user) {
+            // per ogni utente generare dei dettagli
+            $user_details = new UserDetail();
+            //elementi necessari al record
+            $user_details->user_id = $user->id;
+            $user_details->name = $user->name;
+
+            //elementi che possono essere null
+            //usera rand(0, 1) per stabilire casualmente quali campi riempire
+            $user_details->last_name = rand(0, 1) ? $faker->lastName : null;
+            $user_details->username = rand(0, 1) ? $faker->userName : null;
+            $user_details->country = rand(0, 1) ? $faker->country : null;
+            $user_details->city = rand(0, 1) ? $faker->city : null;
+            $user_details->date_of_birth = rand(0, 1) ? $faker->date() : null;
+
+            $user_details->save();
+        }
+    }
+}
